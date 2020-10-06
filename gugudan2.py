c253943c3caae3d479dc5f0718adbe236480d990
selfmaded <cooltime0608@gmail.com>
2020-10-06 15:39:19 +0900

    clear test

--- gugudan2.py
+++ gugudan2.py
@@ -15,14 +15,9 @@ for i in range(1,10):
     print(guguline)
 
 
-
-
 """
 for i in range(1,10):
     for j in range(2,10):
         print("%d * %d = %2d" % (j,i,j*i),end=" ")
     print("\n")
-    
-    
-    test
 """
